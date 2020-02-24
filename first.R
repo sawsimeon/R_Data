365da3e5d7fe432b940bb4e5d227af666be6914c
Saw Simeon <doe_doe207@hotmail.com>
2020-02-24 12:30:08 +0700

    Data

---
+++ first.R
@@ -0,0 +1,11 @@
+print(seq_len(25))
+example_vector <- c(10, 20, 30)
+example_list <- list(a = 10, b= 20, c= 30)
+example_vector[1]
+example_list[1]
+example_vector[[2]]
+
+x <- 1:5
+print(x)
+x <- cumsum(x)
+print(x)
