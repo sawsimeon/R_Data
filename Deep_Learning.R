fba6a2019f6cbe0a8abcbe87c5399074a53433ee
Saw Simeon <doe_doe207@hotmail.com>
2020-03-05 16:51:31 +0700

    Deep Learning

--- Deep_Learning.R
+++ Deep_Learning.R
@@ -158,3 +158,286 @@ history <- model %>% fit(
 )
 
 str(history)
+plot(history)
+
+history_df <- as.data.frame(history)
+str(history_df)
+
+model <- keras_model_sequential() %>%
+layer_dense(units = 16, activation = "relu", input_shape = c(10000)) %>%
+layer_dense(units = 16, activation = "relu") %>%
+layer_dense(units = 1, activation = "sigmoid")
+
+model %>% compile(
+    optimizer = "rmsprop",
+    loss = "binary_crossentropy",
+    metrics = c("accuracy")
+)
+
+model %>% fit(x_train, y_train, epochs = 4, batch_size = 512)
+results <- model %>% evaluate(x_test, y_test)
+
+results
+
+model %>% predict(x_test[1:10, ])
+
+library(keras)
+reuters <- dataset_reuters(num_words = 10000)
+c(c(train_data, train_labels), c(test_data, test_labels)) %<-% reuters
+length(train_data)
+length(test_data)
+
+train_data[[1]]
+
+word_index <- dataset_reuters_word_index()
+reverse_word_index <- names(word_index)
+names(reverse_word_index) <- word_index
+decoded_newswire <- sapply(train_data[[1]], function(index) {
+    word <- if (index >= 3) reverse_word_index[[as.character(index - 3)]]
+    if (!is.null(word)) word else "?"
+})
+train_labels[[1]]
+
+vectorize_sequences <- function(sequences, dimension = 10000) {
+    results <- matrix(0, nrow = length(sequences), ncol = dimension)
+    for (i in 1:length(sequences)) 
+    results[i, sequences[[i]]] <- 1
+    results
+}
+
+x_train <- vectorize_sequences(train_data)
+x_test <- vectorize_sequences(test_data)
+
+to_one_hot <- function(labels, dimension = 46) {
+    results <- matrix(0, nrow = length(labels), ncol = dimension)
+    for (i in 1:length(labels)) 
+    results[i, labels[[i]]] <- 1
+    results
+}
+
+one_hot_train_labels <- to_one_hot(train_labels)
+one_hot_test_labels <- to_one_hot(test_labels)
+
+one_hot_train_labels <- to_categorical(train_labels)
+one_hot_test_labels <- to_categorical(test_labels)
+
+model <- keras_model_sequential() %>%
+layer_dense(units = 64, activation = "relu", input_shape = c(10000)) %>%
+layer_dense(units = 64, activation = "relu") %>%
+layer_dense(units = 46, activation = "softmax")
+
+model %>% compile(
+    optimizer = "rmsprop",
+    loss = "categorical_crossentropy",
+    metrics = c("accuracy")
+)
+
+val_indices <- 1:1000
+
+x_val <- x_train[val_indices, ]
+partial_x_train <- x_train[-val_indices, ]
+
+y_val <- one_hot_train_labels[val_indices, ]
+partial_y_train <- one_hot_train_labels[-val_indices, ]
+
+history <- model %>% fit(
+    partial_x_train,
+    partial_y_train,
+    epochs = 20, 
+    batch_size = 512,
+    validation_data = list(x_val, y_val)
+)
+
+plot(history)
+
+model <- keras_model_sequential() %>%
+layer_dense(units = 64, activation = "relu", input_shape = c(10000)) %>%
+layer_dense(units = 64, activation = "relu") %>%
+layer_dense(units = 46, activation = "softmax")
+
+model %>% compile(
+    optimizer = "rmsprop",
+    loss = "categorical_crossentropy",
+    metrics = c("accuracy")
+)
+
+history <- model %>% fit (
+    partial_x_train, 
+    partial_y_train,
+    epochs = 9,
+    batch_size = 512,
+    validation_data = list(x_val, y_val)
+)
+
+results <- model %>% evaluate(x_test, one_hot_test_labels)
+results
+
+test_labels_copy <- test_labels
+test_labels_copy <- sample(test_labels_copy)
+length(which(test_labels == test_labels_copy)) / length(test_labels)
+
+predictions <- model %>% predict(x_test)
+dim(predictions)
+sum(predictions[1, ])
+which.max(predictions[1, ])
+model %>% compile(
+    optimizer = "rmsprop",
+    loss = "sparse_categorical_crossentropy",
+    metrics = c("accuracy")
+)
+
+model <- keras_model_sequential() %>%
+layer_dense(units = 64, activation = "relu", input_shape  = c(10000))  %>%
+layer_dense(units = 4, activation = "relu") %>%
+layer_dense(units = 46, activation = "softmax")
+
+model %>% compile(
+    optimizer = "rmsprop",
+    loss = "categorical_crossentropy",
+    metrics = c("accuracy")
+)
+
+model %>% fit(
+    partial_x_train,
+    partial_y_train,
+    epochs = 20,
+    batch_size = 128,
+    validation_data = list(x_val, y_val)
+)
+
+
+library(keras)
+
+dataset <- dataset_boston_housing()
+c(c(train_data, train_targets), c(test_data, test_targests)) %<-% dataset
+str(train_data)
+str(test_data)
+
+str(train_targets)
+
+mean <- apply(train_data, 2, mean)
+std <- apply(train_data, 2, sd)
+
+train_data <- scale(train_data, center = mean, scale = std)
+test_data <- scale(test_data, center = mean, scale = std)
+
+build_model <- function() {
+    model <- keras_model_sequential() %>%
+    layer_dense(units = 64, activation = "relu",
+    input_shape = dim(train_data)[[2]]) %>%
+    layer_dense(units = 64, activation = "relu") %>%
+    layer_dense(units = 1)
+
+    model %>% compile(
+        optimizer = "rmsprop",
+        loss = "mse",
+        metrics = c("mae")
+    )
+}
+
+k <- 4
+indices <- sample(1:nrow(train_data))
+folds <- cut(indices, breaks = k, labels = FALSE)
+
+num_epochs <- 100
+all_scores <- c()
+for (i in 1:k) {
+    cat("processing fold #", i, "\n")
+
+    val_indices <- which(folds == i, arr.ind = TRUE)
+    val_data <- train_data[val_indices, ]
+    val_targets <- train_targets[val_indices]
+
+    partial_train_data <- train_data[-val_indices, ]
+    partial_train_targets <- train_targets[-val_indices]
+
+    model <- build_model()
+
+    model %>% fit(partial_train_data, partial_train_targets, 
+    epochs = num_epochs, batch_size = 1, verbose = 0)
+    results <- model %>% evaluate(val_data, val_targets, verbose = 0)
+    all_scores <- c(all_scores, results$mae)
+}
+
+all_scores
+mean(all_scores)
+
+
+
+num_epochs <- 500
+all_mae_histories <- NULL
+
+for (i in 1:k) {
+    cat("processing fold #", i, "\n")
+
+    val_indices <- which(folds == i, arr.ind = TRUE)
+    val_data <- train_data[val_indices, ]
+    val_targets <- train_targets[val_indices]
+
+    partial_train_data <- train_data[-val_indices, ]
+    partial_train_targets <- train_targets[-val_indices]
+
+    model <- build_model()
+
+    histor <- model %>% fit(partial_train_data, partial_train_targets, 
+    validation_data <- list(val_data, val_targets),
+    epochs = num_epochs, batch_size = 1, verbose = 0)
+    mae_history <- history$metrics$val_mean_absolute_error
+    all_mae_histories <- rbind(all_mae_histories, mae_history)
+    
+}
+
+model <- build_model()
+
+model %>% fit(train_data, train_targets, 
+epochs = 80, batch_size = 16, verbose = 0)
+result <- model %>% evaluate(test_data, train_targets)
+
+library(keras)
+model <- keras_model_sequential() %>%
+layer_conv_2d(filters = 32, kernel_size = c(3, 3), activation = "relu",
+input_shape = c(28, 28, 1)) %>%
+layer_max_pooling_2d(pool_size = c(2, 2)) %>%
+layer_conv_2d(filters = 64, kernel_size = c(3, 3), activation = "relu") %>%
+layer_max_pooling_2d(pool_size = c(2, 2)) %>%
+layer_conv_2d(filters = 64, kernel_size = c(3, 3), activation = "relu")
+
+model
+
+model <- model %>%
+layer_flatten() %>%
+layer_dense(units = 64, activation = "relu") %>%
+layer_dense(units = 10, activation = "softmax")
+model
+
+mnist <- dataset_mnist()
+c(c(train_images, train_labels), c(test_images, test_labels)) %<-% mnist
+
+train_images <- array_reshape(train_images, c(60000, 28, 28, 1))
+train_images <- train_images/ 255
+
+test_images <- array_reshape(test_images, c(10000, 28, 28, 1))
+test_images <- test_images/ 255
+
+train_labels <- to_categorical(train_labels)
+test_labels <- to_categorical(test_labels)
+
+model %>% compile(
+    optimizer = "rmsprop", 
+    loss = "categorical_crossentropy",
+    metrics = c("accuracy")
+)
+
+model %>% fit(train_images, train_labels, epochs = 5, batch_size = 64)
+
+results <- model %>% evaluate(test_images, test_labels)
+results
+
+model_no_max_pool <- keras_model_sequential() %>%
+layer_conv_2d(filters = 32, kernel_size = c(3, 3), activation = "relu",
+input_shape = c(28, 28, 1)) %>%
+layer_conv_2d(filters = 64, kernel_size = c(3, 3), activation = "relu") %>%
+layer_conv_2d(filters = 64, kernel_size = c(3, 3), activation = "relu")
+model_no_max_pool
+
+## Page 128 of 341
