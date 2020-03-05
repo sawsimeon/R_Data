483073234a64831d29b7bbe1948fea239161497e
Saw Simeon <doe_doe207@hotmail.com>
2020-03-05 14:56:41 +0700

    Deep Learning

--- Deep_Learning.R
+++ Deep_Learning.R
@@ -96,3 +96,65 @@ dim(x)
 
 x <- t(x)
 dim(x)
+
+## page 43/ 341
+library(keras) 
+imdb <- dataset_imdb(num_words = 10000)
+c(c(train_data, train_labels), c(test_data, test_labels)) %<-% imdb
+str(train_data[[1]])
+train_labels[[1]]
+max(sapply(train_data, max))
+word_index <- dataset_imdb_word_index()
+reverse_word_index <- names(word_index)
+
+vectorize_sequences <- function(sequences, dimension = 10000) {
+    results <- matrix(0, nrow = length(sequences), ncol = dimension)
+    for (i in 1:length(sequences)) 
+    results[i, sequences[[i]]] <- 1
+    results
+}
+
+x_train <- vectorize_sequences(train_data)
+x_test <- vectorize_sequences(test_data)
+
+str(x_train[1, ])
+
+y_train <- as.numeric(train_labels)
+y_test <- as.numeric(test_labels)
+
+library(keras)
+
+model <- keras_model_sequential() %>%
+layer_dense(units = 16, activation = "relu", input_shape = c(10000)) %>%
+layer_dense(units = 16, activation = "relu") %>%
+layer_dense(units = 1, activation = "sigmoid")
+
+model %>% compile(
+    optimizer = "rmsprop",
+    loss = "binary_crossentropy",
+    metrics = c("accuracy")
+)
+
+val_indices <- 1:10000
+
+x_val <- x_train[val_indices, ]
+partial_x_train <- x_train[-val_indices, ]
+
+y_val <- y_train[val_indices]
+partial_y_train <- y_train[-val_indices]
+
+model %>% compile(
+    optimizer = "rmsprop",
+    loss = "binary_crossentropy",
+    metrics = c("accuracy")
+)
+
+history <- model %>% fit(
+    partial_x_train,
+    partial_y_train, 
+    epochs= 20,
+    batch_size = 512, 
+    validation_data = list(x_val, y_val)
+)
+
+str(history)
