54ea777f73e6a165e6e0793179d753495a8bddb0
Saw Simeon <doe_doe207@hotmail.com>
2020-03-10 17:05:44 +0700

    dee learning

--- Deep_Learning.R
+++ Deep_Learning.R
@@ -671,6 +671,48 @@ model <- application_vgg16(
     include_top = FALSE
 )
 library(keras)
-model <- load_model_hdf5("vgg16_weights_tf_dim_ordering_tf_kernels_notop.h5")
+model <- load_model_hdf5("vgg16.h5")
 
-### page of 159 / 341
+imdb_dir <- "C:/Users/Saw/Downloads/aclImdb"
+train_dir <- file.path(imdb_dir, "train")
+
+labels <- c()
+texts <- c()
+
+for (label_type in c("neg", "pos")) {
+    label <- switch(label_type, neg = 0, pos = 1)
+    dir_name <- file.path(train_dir, label_type)
+    for (fname in list.files(dir_name, pattern = glob2rx("*.txt"),
+    full.names = TRUE)) {
+        text <- c(texts, readChar(fname, file.info(fname)$size))
+        labels <- c(labels, label)
+    }
+}
+
+library(keras)
+maxlen <- 100
+training_samples <- 200
+validation_samples <- 10000
+max_words <- 10000
+
+tokenizer <- text_tokenizer(num_words = max_words) %>%
+fit_text_tokenizer(texts)
+
+sequences <- texts_to_sequences(tokenizer, texts)
+
+word_index <- tokenizer$word_index
+
+cat("Found", length(word_idnex), "unique tokens.\n")
+
+data <- pad_sequences(sequences, maxlen = maxlen)
+
+labels <- as.array(labels)
+cat("Shape of data tensor:", dim(data), "\n")
+cat("Shape of label tensor:", dim(labels), "\n")
+
+indices <- sample(1:rnow(data))
+training_indices <- indices[1:training_samples]
+library(devtools)
+
+### page 184 / 341
+
