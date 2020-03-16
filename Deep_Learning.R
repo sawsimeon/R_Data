4f5cf28ce3325c39f01a3f0df9ff5095a3791322
Saw Simeon <doe_doe207@hotmail.com>
2020-03-16 16:37:07 +0700

    deep learning

--- Deep_Learning.R
+++ Deep_Learning.R
@@ -716,3 +716,86 @@ library(devtools)
 
 ### page 184 / 341
 
+library(keras)
+model <- keras_model_sequential() %>% 
+layer_embedding(input_dim = 10000, output_dim = 32) %>% 
+layer_simple_rnn(units = 32)
+summary(model)
+
+model <- keras_model_sequential() %>% 
+layer_embedding(input_dim = 10000, output_dim = 32) %>% 
+layer_simple_rnn(units = 32, return_sequences = TRUE)
+summary(model)
+
+model <- keras_model_sequential() %>% 
+layer_embedding(input_dim = 10000, output_dim = 32) %>% 
+layer_simple_rnn(units = 32, return_sequences = TRUE) %>% 
+layer_simple_rnn(units = 32, return_sequences = TRUE) %>% 
+layer_simple_rnn(units = 32, return_sequences = TRUE) %>% 
+layer_simple_rnn(units = 32)
+
+summary(model)
+
+## Preprocess the Data 
+
+library(keras)
+
+max_features <- 10000
+
+maxlen <- 500
+
+batch_size = 32
+
+cat("Loading data...\n")
+imdb <- dataset_imdb(num_words = max_features)
+c(c(input_train, y_train), c(input_test, y_test)) %<-% imdb
+cat(length(input_train), "train sequences\n")
+cat(length(input_test), "test sequences\n")
+cat("Pad sequences (samples x times) \n")
+input_train <- pad_sequences(input_train, maxlen = maxlen)
+input_test <- pad_sequences(input_test, maxlen = maxlen)
+cat("input_train shape:", dim(input_train), "\n")
+cat("input_test shape:", dim(input_test), "\n")
+
+model <- keras_model_sequential() %>% 
+layer_embedding(input_dim = max_features, output_dim = 32) %>% 
+layer_simple_rnn(units = 32) %>% 
+layer_dense(units = 1, activation = "sigmoid")
+
+model %>% compile(
+    optimizer = "rmsprop", 
+    loss = "binary_crossentropy", 
+    metrics = c("acc")
+)
+
+history <- model %>% fit(
+    input_train, y_train, 
+    epochs = 10,
+    batch_size = 128, 
+    validation_split = 0.2
+)
+
+plot(history)
+
+model <- keras_model_sequential() %>% 
+layer_embedding(input_dim = max_features, output_dim = 32) %>% 
+layer_lstm(units = 32) %>% 
+layer_dense(units = 1, activation = "sigmoid")
+
+model %>% compile(
+    optimizer = "rmsprop",
+    loss = "binary_crossentropy", 
+    metrics = c("acc")
+)
+
+history <- model %>% fit(
+    input_train, y_train,
+    epochs = 10,
+    batch_size = 128, 
+    validatioN_split = 0.2
+)
+
+plot(history)
+
+library(tabble)
+
