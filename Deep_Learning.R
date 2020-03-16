6bc514b5873ff1167d679e156e8aeb10dc0858f4
Saw Simeon <doe_doe207@hotmail.com>
2020-03-16 18:08:56 +0700

    deep learning

--- Deep_Learning.R
+++ Deep_Learning.R
@@ -797,5 +797,120 @@ history <- model %>% fit(
 
 plot(history)
 
-library(tabble)
+library(tibble)
+library(readr)
+
+data_dir <- c("C:/Users/Saw/Downloads/jena_climate")
+fname <- file.path(data_dir, "jena_climate_2009_2016.csv")
+data <- read_csv(fname)
+glimpse(data)
+library(ggplot2)
+ggplot(data, aes(x = 1:nrow(data), y = `T (degC)`)) +
+geom_line()
+
+head(data)
+
+ggplot(data[1:1440, ], aes(x = 1:1440, y = `T (degC)`)) +
+geom_line()
+
+sequence_generator <- function(start) {
+    value <- start - 1
+    function() {
+        value <<- value + 1
+        value
+    }
+}
+
+gen <- sequence_generator(10)
+gen()
+gen()
+
+data <- data.matrix(data[, -1])
+train_data <- data[1:200000,]
+mean <- apply(train_data, 2, mean)
+std <- apply(train_data, 2, sd)
+data <- scale(data, center = mean, scale = std)
+
+generator <- function(data, lookback, delay, min_index, max_index,
+ shuffle = FALSE, batch_size = 128, step = 6) {
+ if (is.null(max_index))
+ max_index <- nrow(data) - delay - 1
+ i <- min_index + lookback
+ function() {
+ if (shuffle) {
+ rows <- sample(c((min_index+lookback):max_index), size = batch_size)
+ } else {
+ if (i + batch_size >= max_index)
+ i <<- min_index + lookback
+ rows <- c(i:min(i+batch_size, max_index))
+ i <<- i + length(rows)
+ }
+ samples <- array(0, dim = c(length(rows),
+ lookback / step,
+ dim(data)[[-1]]))
+ targets <- array(0, dim = c(length(rows)))
+ for (j in 1:length(rows)) {
+ indices <- seq(rows[[j]] - lookback, rows[[j]],
+ length.out = dim(samples)[[2]])
+ samples[j,,] <- data[indices,]
+ targets[[j]] <- data[rows[[j]] + delay,2]
+ }
+ list(samples, targets)
+ }
+}
+
+
+lookback <- 1440
+step <- 6
+delay <- 144
+batch_size = 128
+
+train_gen <- generator(
+    data, 
+    lookback = lookback,
+    delay = delay,
+    min_index = 1,
+    max_index = 200000,
+    shuffle = TRUE,
+    step = step,
+    batch_size = batch_size
+)
+
+val_gen <- generator(
+    data, 
+    lookback = lookback,
+    delay = delay,
+    min_index = 200001,
+    max_index = 30000, 
+    step = step,
+    batch_size = batch_size
+)
+
+test_gen <- generator(
+    data, 
+    lookback = lookback,
+    delay = delay,
+    min_index = 300001,
+    max_index = NULL,
+    step = step,
+    batch_size = batch_size
+)
+
+val_steps <- (300000-200001-lookback) / batch_size
+test_steps <- (nrow(data) - 300001 - lookback) / batch_size
+
+evaluate_naive_method <- function() {
+    batch_maes <- c()
+    for (step in 1:val_steps) {
+        c(samples, targets) %<-% val_gen()
+        preds <- samples[, dim(samples)[[2]],2]
+        mae <- mean(abs(preds - targets))
+        batch_maes <- c(batch_maes, mae)
+    }
+    print(mean(batch_maes))
+}
+
+evaluate_naive_method()
+
+#### 206/341
 
