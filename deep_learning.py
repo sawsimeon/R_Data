be0e948c15afafac0ca5b7faecb40ce2b8d9681d
Saw Simeon <doe_doe207@hotmail.com>
2020-04-03 18:49:03 +0700

    Deep Learning

--- deep_learning.py
+++ deep_learning.py
@@ -7,5 +7,6 @@ pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris
  names=['sepal_length', 'sepal_width','petal_length', 'petal_width', 'species'])
 
  dataset['species'] = pd.Categorical(dataset['species']).codes
+ dataset = dataset.sample(frac = 1, random_state = 1234)
  
 
