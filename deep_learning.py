9b2e61f9d924c479261a542f599c63be85d55450
Saw Simeon <doe_doe207@hotmail.com>
2020-04-03 12:29:35 +0700

    deep learning ipython notebook

--- deep_learning.py
+++ deep_learning.py
@@ -6,3 +6,6 @@ dataset =
 pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data',
  names=['sepal_length', 'sepal_width','petal_length', 'petal_width', 'species'])
 
+ dataset['species'] = pd.Categorical(dataset['species']).codes
+ 
+
