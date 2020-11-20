badfa6cf29e482ff104ba19d24b07c0701410bb6
Saw Simeon <doe_doe207@hotmail.com>
2020-11-20 16:39:21 +0700

    more practice

---
+++ Multiplication.java
@@ -0,0 +1,4 @@
+public class Multiplication {
+ for(int i=1; i<=10:i++)
+ System.out.printf("%d * %d = %d", 5, i, 5 * i).println();   
+}
--- TemperatureConverter.java
+++ TemperatureConverter.java
@@ -1,22 +1,22 @@
 public class TemperatureConverter {
-
-
-​public static String convertTemp (float temperature, char convertTo) {
-
-    ​if (convertTo=='F'){
-         ​return "The temperature in Fahrenheit is " + (9*temperature/5 + 32);
-     ​} else if(convertTo=='C') {
-        ​return "The temperature in Celsius is " + (temperature - 32) * 5/9;
-    ​}  else{
-        ​return "You can enter either F or C as convertTo argument";
-    ​}
-​
+    public static float convertTemp1 (float temperature,
+        char convertTo) {
+        return convertTo;}
+        public static String convertTemp (float temperature, char convertTo) {
+    
+         if (convertTo=='F'){
+         return "The temperature in Fahrenheit is " + (9*temperature/5 + 32);
+        } else if(convertTo=='C') {
+         return "The temperature in Celsius is " + (temperature - 32) * 5/9;
+        }  else{
+         return "You can enter either F or C as convertTo argument";
+    
     }
-
+    }
+    
     public static void main(String[] args) {
-
-        System.out.println("Converting 21C to Fahrenheit. " +  convertTemp(21,'F'));
-        System.out.println("Converting 70F to Celsius. " +  convertTemp(70,'C'));
-
+    
+    System.out.println("Converting 21C to Fahrenheit. " +  convertTemp(21,'F', 0));
+    System.out.println("Converting 70F to Celsius. " + convertTemp(70,'C', 0));
     }
-}
+    }
